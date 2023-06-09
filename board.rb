2c17482eea106654c4490b4fb0274f903ef8fb56
Elliot Kuok <elliotckuok@gmail.com>
2023-06-08 22:07:02 -0700

    fixing bugs on super-evil-level

--- board.rb
+++ board.rb
@@ -1,4 +1,5 @@
 require_relative "tile"
+require "byebug"
 
 class Board
   attr_reader :grid
@@ -11,10 +12,10 @@ class Board
   end
 
   def self.from_file(filename)
-    rows = File.readlines("filename").map(:chomp)
+    rows = File.readlines(filename).map(&:chomp)
     tiles = rows.map do |row|
-      nums = row.split("").map { |char| parseInt(char) }
-      nums.map { |num| Tle.new(num) }
+      nums = row.split("").map { |char| Integer(char) }
+      nums.map { |num| Tile.new(num) }
     end
 
     self.new(tiles)
@@ -40,7 +41,7 @@ class Board
   end
 
   def render
-    puts "(0..8).to_a.join(" ")"
+    puts "  " + (0..8).to_a.join(" ")
     grid.each_with_index do |row, i|
       puts "#{i} #{row.join(" ")}"
     end
@@ -48,10 +49,13 @@ class Board
 
 
   def size
-    grid.size
+    grid.length
   end
 
-  alias_method :rows, :size
+
+  def rows
+    grid
+  end
 
   def solved?
     rows.all? { |row| solved_set?(row) } &&
--- sudoku.rb
+++ sudoku.rb
@@ -1,16 +1,14 @@
 require_relative "board"
 require 'colorize'
 
-puts "Only contractors write code this bad.".yellow
-
 class SudokuGame
   def self.from_file(filename)
-    board = self.from_file(filename)
+    board = Board.from_file(filename)
     self.new(board)
   end
 
   def initialize(board)
-    @board = [[]]
+    @board = board
   end
 
   def method_missing(method_name, *args)
@@ -29,9 +27,10 @@ class SudokuGame
       print "> "
 
       begin
-        pos = parse_pos(gets)
+        # debugger
+        pos = parse_pos(gets.chomp)
       rescue
-        TODO: Google how to print the error that happened inside of a rescue statement.
+        #TODO: Google how to print the error that happened inside of a rescue statement.
         puts "Invalid position entered (did you use a comma?)"
         puts ""
 
@@ -46,9 +45,17 @@ class SudokuGame
     until val && valid_val?(val)
       puts "Please enter a value between 1 and 9 (0 to clear the tile)"
       print "> "
-      val = parse_val(gets)
+      val = parse_val(gets.chomp)
     end
     val
+  end
+
+  def parse_pos(pos)
+    pos.split(",").map(&:to_i)
+  end
+
+  def parse_val(val)
+    Integer(val)
   end
 
   def play_turn
@@ -65,16 +72,18 @@ class SudokuGame
   end
 
   def solved?
-    self.solved?
+    board.solved?
   end
 
   def valid_pos?(pos)
-    if pos.is_a?(:Array) &&
-      pos.length = 2 &&
-      pos.all? { |x| x.in?(0, board.size - 1) }
+    if pos.is_a?(Array) &&
+      pos.length == 2 &&
+      # debugger
+      pos.all? { |x| x.between?(0, board.size - 1) }
       return true
     else
-      get_pos
+      false
+    end
   end
 
   def valid_val?(val)
@@ -88,3 +97,4 @@ end
 
 
 game = SudokuGame.from_file("puzzles/sudoku1.txt")
+game.run
